8dde47270b44b3331d7591a9221d2da2a8339777
Donex fience <149715881+donexfience@users.noreply.github.com>
2024-05-31 01:19:33 +0530

    Day-3 last commit Sorting algorithms finished

---
+++ Sorting Algorithms/SelectionSort.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < array.length - 1; i++) {
+  let min = i;
+  for (let j = i + 1; j < array.length; j++) {
+    if (array[j] < array[min]) {
+      min = j;
+    }
+  }
+  if (min != i) {
+    let temp 
+  }
+}
